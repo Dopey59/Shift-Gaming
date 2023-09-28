c412104e45d8a3f235633337460d987d4679fdb1
Dopey59 <william.penet@epitech.eu>
2023-09-28 02:55:51 +0200

    [+] Added new icons feature for instant-gaming comp

--- client/src/components/homepage.js
+++ client/src/components/homepage.js
@@ -7,6 +7,8 @@ import Footer from '../components/footer';
 import Navbar from '../components/navbar';
 import FeaturesSteelseries from '../components/featuresSteelseries';
 import FeaturesLogitech from '../components/featuresLogitech'
+import InstantFeature from '../components/instant-gaming'
+
 
 
 
@@ -30,11 +32,13 @@ export default function Homepage(){
             </div>
 
             <div>
-                <div className=' flex py-16 flex-col justify-center items-center bg-blue-900 bg-gradient-to-b from-zinc-900/[.15] via-transparent sm:text-3xl text-xl text-center text-white'>
+                {/* <div className=' flex py-16 flex-col justify-center items-center bg-blue-900 bg-gradient-to-b from-zinc-900/[.15] via-transparent sm:text-3xl text-xl text-center text-white'>
                     <h1 >Obtiens tout tes jeux moins chers et rapidement avec Instant Gaming.</h1>
-                </div>
+                </div> */}
             </div>
-                <Carousel/>
+            <div className='bg-gradient-to-tr from-zinc-900 to-slate-900 text-white'>
+                <InstantFeature/>
+            </div>
                 
             <div className='bg-gray-900'>
                 <Perf />
