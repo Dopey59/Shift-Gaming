ba34469dbf44f48f29e6dceb40fd8ec23ae6f30c
Dopey59 <william.penet@epitech.eu>
2023-10-03 22:53:05 +0200

    [~] Useeffect for caroussel added and working

--- client/src/components/carousel.js
+++ client/src/components/carousel.js
@@ -1,64 +1,64 @@
+import React, { useEffect } from 'react';
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 export default function Carousel() {
-  const swiper = new Swiper('.swiper', {
-    // Paramètres optionnels
-    direction: 'horizontal',
-    loop: true,
-
-    // Si nous avons besoin de la pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-
-    // Flèches de navigation
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+  useEffect(() => {
+    const swiper = new Swiper('.swiper', {
+      direction: 'horizontal',
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
+  }, []); // Le tableau vide [] signifie que cet effet ne sera exécuté qu'une seule fois après le chargement initial du composant.
 
   return (
     <div className="swiper">
-      {/* Conteneur requis supplémentaire */}
       <div className="swiper-wrapper">
-        {/* Diapositives */}
         <div className="swiper-slide">
           <img
             src="https://images.unsplash.com/photo-1580610447943-1bfbef5efe07?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
             alt="Slide 1"
+            className=''
           />
-          <div className="absolute flex flex-col gap-2  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
+          <div className="absolute flex flex-col gap-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
             <h1 className="sm:text-3xl text-xl font-bold">Pour une communauté de passionnés</h1>
             <p className="sm:text-lg text-sm">De belles choses arrivent.</p>
           </div>
         </div>
 
         <div className="swiper-slide">
-            <img
-                src="https://images.unsplash.com/photo-1482855549413-2a6c9b1955a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-                alt="Slide 2 - manette xbox series s"
-            />
-            <div className="absolute top-1/2 left-1/2 flex flex-col gap-2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
-                <h1 className="text-xl sm:text-3xl font-bold">Plus on est d'fous, plus on game</h1>
-                <p className="sm:text-lg text-sm">Shift Gaming s'adresse aussi aux joueurs consoles.</p>
-            </div>
+          <img
+            src="https://images.unsplash.com/photo-1482855549413-2a6c9b1955a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+            alt="Slide 2 - manette xbox series s"
+            className=''
+          />
+          <div className="absolute top-1/2 left-1/2 flex flex-col gap-2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
+            <h1 className="text-xl sm:text-3xl font-bold">Plus on est d'fous, plus on game</h1>
+            <p className="sm:text-lg text-sm">Shift Gaming s'adresse aussi aux joueurs consoles.</p>
+          </div>
         </div>
 
         <div className="swiper-slide">
-            <img
-                src="https://images.unsplash.com/photo-1636487658616-14850c8f496c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80"
-                alt="Slide 3 - gamer"
-            />
-            <div className="absolute top-1/2 left-1/2 flex flex-col gap-2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
-                <h1 className="sm:text-3xl text-xl font-bold">"Le guild ultime pour les gamers"</h1>
-                <p className="sm:text-lg text-sm">Des trucs & astuces pour vous aider pas à pas.</p>
-            </div>
-        </div> 
-    </div>
-
-      
+          <img
+            src="https://images.unsplash.com/photo-1661347561879-c9ab77bac89f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+            alt="Slide 3 - gamer"
+            className='brightness-50'
+          />
+          <div className="absolute top-1/2 left-1/2 flex flex-col gap-2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
+            <h1 className="sm:text-3xl text-xl font-bold">"Le guild ultime pour les gamers"</h1>
+            <p className="sm:text-lg text-sm">Des trucs & astuces pour vous aider pas à pas.</p>
+          </div>
+        </div>
+      </div>
 
       {/* Si nous avons besoin de la pagination */}
       <div className="swiper-pagination"></div>
